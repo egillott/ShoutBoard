77fba8abaae1e8ec113a2622ce650f3ee873d16e
Wen Han Huang <huang690@purdue.edu>
2016-04-25 03:34:05 +0000

    probably works

--- MyHandler.py
+++ MyHandler.py
@@ -5,6 +5,6 @@ except ImportError:
 	from http.server import SimpleHTTPRequestHandler as Handler
 	from http.server import HTTPServer as Server
 
-class MyHandler(Handler):
+class MyHandler(Handler, object):
 	def do_GET(self):
 		super(MyHandler, self).do_GET()
