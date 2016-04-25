4d6b9d3c872e0b3ccb9b31eb9580518c39b83154
Brandon Xia <brandonxia01@gmail.com>
2016-04-25 22:23:52 +0000

    draw a dot

--- static/index.js
+++ static/index.js
@@ -5,17 +5,31 @@
 /*eslint-env browser */
 /*globals ActiveXObject */
 
-var sizing = document.getElementById('whatANiceCanvas');
-sizing.width = window.innerWidth;
-sizing.height = window.innerHeight;
+var canvas = document.getElementById('whatANiceCanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var penSize = 10;
+
+var context = canvas.getContext('2d');
+
+var drawPoint = function(e) {
+	context.beginPath();
+	context.arc(e.offSetX, e.offSetY, penSize, 0, Math.PI * 2);
+	context.fill();
+}
+
+
+
+canvas.addEventListener('mousedown', drawPoint)
 
 function resize_canvas() {
-			var canvas = document.getElementById("whatANiceCanvas");
-			canvas.width = window.innerWidth;
-			canvas.height = window.innerHeight;
+			var sizing = document.getElementById("whatANiceCanvas");
+			sizing.width = window.innerWidth;
+			sizing.height = window.innerHeight;
 			
-			var ctx = canvas.getContext("2d");
+			var ctx = sizing.getContext("2d");
 			ctx.moveTo(0,0);
-			ctx.lineTo(canvas.width, canvas.height);
+			ctx.lineTo(sizing.width, sizing.height);
 			ctx.stroke();
 }
