bec7bcf26a4ef5beaab0d7fd6930d50866896e91
Wen Han Huang <huang690@purdue.edu>
2016-04-25 01:30:36 +0000

    ok

---
+++ static/index.js
@@ -0,0 +1,10 @@
+// index.js
+
+// request message on server
+//Calls SimpleServlet to get the "Hello World" message
+/*eslint-env browser */
+/*globals ActiveXObject */
+
+var sizing = document.getElementById('whatANiceCanvas');
+sizing.width = window.innerWidth;
+sizing.height = window.innerHeight;
