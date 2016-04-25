b258b6f168290aa78527c63144309d20de8400bd
Wen Han Huang <huang690@purdue.edu>
2016-04-25 01:09:27 +0000

    Revert "test"
    This reverts commit c68282bc7f4e0f59b26b6cc4d9c68812c4a655ba.

--- server.py
+++ server.py
@@ -1,46 +1,21 @@
-#import os
-#try:
-#  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-#  from SocketServer import TCPServer as Server
-#except ImportError:
-#  from http.server import SimpleHTTPRequestHandler as Handler
-#  from http.server import HTTPServer as Server
-#
-## Read port selected by the cloud for our application
-#PORT = int(os.getenv('PORT', 8000))
-## Change current directory to avoid exposure of control files
-#os.chdir('static')
-#
-#class MyServer(Server) {
-#	
-#}
-#
-#httpd = Server(("", PORT), Handler)
-#
-#try:
-#  print("Start serving at port %i" % PORT)
-#  httpd.serve_forever()
-#except KeyboardInterrupt:
-#  pass
-#httpd.server_close()
-
-import time
 import os
-from http.server import HTTPServer
-from MyHTTPRequestHandler import MyHTTPRequestHandler
+try:
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
+except ImportError:
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
 
-hostName = ""  # "localhost"
-hostPort = int(os.getenv('PORT', 8000))
-
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+# Change current directory to avoid exposure of control files
 os.chdir('static')
 
-myServer = HTTPServer((hostName, hostPort), MyHTTPRequestHandler)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
-
+httpd = Server(("", PORT), Handler)
 try:
-    myServer.serve_forever()
+  print("Start serving at port %i" % PORT)
+  httpd.serve_forever()
 except KeyboardInterrupt:
-    pass
+  pass
+httpd.server_close()
 
-myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
