4662836f50020e67230253e2923ffcdf940056f0
Wen Han Huang <huang690@purdue.edu>
2016-04-25 22:10:03 +0000

    moving code

--- static/index.js
+++ static/index.js
@@ -8,3 +8,14 @@
 var sizing = document.getElementById('whatANiceCanvas');
 sizing.width = window.innerWidth;
 sizing.height = window.innerHeight;
+
+function resize_canvas() {
+			var canvas = document.getElementById("whatANiceCanvas");
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+			
+			var ctx = canvas.getContext("2d");
+			ctx.moveTo(0,0);
+			ctx.lineTo(canvas.width, canvas.height);
+			ctx.stroke();
+}
