ba0c1572a249c956a54caae474d1f9aecd740cd0
Wen Han Huang <huang690@purdue.edu>
2016-04-26 03:30:33 +0000

    k

--- MyHandler.py
+++ MyHandler.py
@@ -12,13 +12,11 @@ class MyHandler(Handler, object):
 		
 	def do_POST(self):
 		print("url path" ,self.path)
-		print(self.headers)
-        content_len = int(self.headers.get('Content-Length', 0))
-        print("content_len",content_len)
+#        content_len = int(self.headers.get('Content-Length', 0))
+#        print("content_len",content_len)
 #        print("lots of datas", self.rfile.read(content_len))
 #		post_body = self.rfile.read(content_len).decode("utf-8")
 #		result = parse_qs(post_body)
 #        for k in result:
 #            print(result[k])
-		
 		super(MyHandler, self).do_POST()
